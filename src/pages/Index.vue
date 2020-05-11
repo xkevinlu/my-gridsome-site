4e5096195ab9850654a7b9840692c88b74a41121
xkevinlu <xkevinlu@gmail.com>
2020-05-11 00:07:58 -0700

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -7,7 +7,7 @@
     <h1>Hello, world!</h1>
 
     <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
+Kevin's first site
     </p>
 
     <p class="home-links">

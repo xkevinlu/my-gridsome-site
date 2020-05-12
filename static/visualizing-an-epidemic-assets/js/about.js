c4eb3079682c89b1ca48135eb1aa5ccee007c8ed
xkevinlu <xkevinlu@gmail.com>
2020-05-11 21:05:03 -0700

    updates

--- static/visualizing-an-epidemic-assets/js/about.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("navToggle").onclick = function navToggle() {
-    var x = document.getElementById("topnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
--- static/visualizing-an-epidemic-assets/js/colorList.js
+++
@@ -1,5 +0,0 @@
-export const colorList = {
-    healthy: '#63c2db',
-    infected: '#f66654',
-    removed: 'gray',
-  };

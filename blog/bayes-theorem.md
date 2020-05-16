98443948854b91a3add72ea11a98dd83863b8c15
xkevinlu <xkevinlu@gmail.com>
2020-05-16 16:22:50 -0700

    bayes-theorem

---
+++ blog/bayes-theorem.md
@@ -0,0 +1,43 @@
+
+---
+
+title: Bae's Theorem - How to statistically tell if she's into you
+
+date: 2020-05-17
+
+author: Kevin
+
+image: https://www.colorsandcode.com/thumbnail-visualizing-an-epidemic.PNG
+
+---
+<style>
+.iframe-1 {
+border:0;
+width: 100%;
+height: 500px;
+margin: auto;
+scrolling: no;
+}
+</style>
+Meet Shannon.
+![shannon-stick-figure](www.colorsandcode.com/bayes-theorem-assets/stick-figure-girl-1.png)
+You're secretly in love with Shannon. You guys are friends, though you have no idea if Shannon is into you. Shannon typically shows no interest in 95% of people. However when she is interested and wants to "chill" with someone, she would invite them over for Netflix 90% of the time. 
+
+One day, Shannon invites you over for "Netflix". You are super excited. On the other hand, maybe she has friend zoned you and just wants to actually watch Netflix, Though because Shannon doesn't even own a Netflix account and typically does other things with her friends, you think there's only a 20% chance she asks a friend to watch Netflix.
+
+**Is Shannon inviting you over for chill, or just Netflix?**
+
+At first it seems highly likely Shannon wants to chill, but statistically its far more likely you're going to end up just watching Netflix. Why? Let's look at a diagram of the last 10 times Shannon hung out with 20 of her friends.
+
+<iframe  class="iframe-1"  src="https://www.colorsandcode.com/bayes-theorem-assets/bayes.html"></iframe>
+
+Let's first look at how Shannon behaves with friends vs. people she wants to chill with. With the one friend she wanted to chill with, 9/10 times there would be Netflix.  Out of the last 10 times Shannon hung out with just a friend, only 2/10 times there would be Netflix. However, we have to remember that Shannon isn't into 95% of people. Thus, if we look across 20 people that Shannon is close to, 19 of them are friends. And if we look at the last 10 times she hung out with these 20 people, we can see that out of the 47 times there were Netflix, 38 of those times were with a friend. Thus, if Shannon invites you to Netflix, its 38/47 = 81% chance you'll just watch Netflix.
+
+
+Source inspirations: 
+
+Bayes Theorem 3blue1brown
+[https://www.youtube.com/watch?v=HZGCoVF3YvM]
+
+Bayesian Trap Veritasium
+[https://www.youtube.com/watch?v=R13BD8qKeTg&t=302s]
--- static/bayes-theorem-assets/scripts.js
+++ static/bayes-theorem-assets/scripts.js
@@ -1,8 +1,8 @@
 const colorsList = [
-    "#AB87FF",
-    "#96E072",
-    "#DFFFB8",
-    "#F0DCFF"
+    "#E63946",
+    "#A8DADC",
+    "#F1FAEE",
+    "#FDB9B5"
 ]
 
 
@@ -13,10 +13,10 @@ const ctx = canvas.getContext('2d');
 canvas.innerWidth = canvas.width;
 canvas.innerHeight = canvas.height;
 
-window.addEventListener("resize", function() {
+window.addEventListener("resize", function () {
     canvas.innerWidth = canvas.width;
     canvas.innerHeight = canvas.height;
-  })
+})
 
 let ph = 0.05;
 let peh = 0.4;
@@ -24,34 +24,34 @@ let penh = 0.2;
 let ratio = 0;
 
 function draw() {
-//P(-H)
-ctx.fillStyle = colorsList[2];
-ctx.fillRect(0,0,canvas.width, canvas.height)
+    //P(-H)
+    ctx.fillStyle = colorsList[2];
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
 
-//P(H)
-ctx.fillStyle = colorsList[3];
-ctx.fillRect(0,0,canvas.width*ph, canvas.height)
+    //P(H)
+    ctx.fillStyle = colorsList[3];
+    ctx.fillRect(0, 0, canvas.width * ph, canvas.height)
 
-//P(E|H)
-ctx.fillStyle = colorsList[0];
-ctx.fillRect(0,canvas.height-(canvas.height*peh), canvas.width*ph, canvas.height*peh );
+    //P(E|H)
+    ctx.fillStyle = colorsList[0];
+    ctx.fillRect(0, canvas.height - (canvas.height * peh), canvas.width * ph, canvas.height * peh);
 
-//P(E/-H)
-ctx.fillStyle = colorsList[1];
-ctx.fillRect(canvas.width*ph,canvas.height-(canvas.height*penh), canvas.width*(1-ph), canvas.height*penh );
+    //P(E/-H)
+    ctx.fillStyle = colorsList[1];
+    ctx.fillRect(canvas.width * ph, canvas.height - (canvas.height * penh), canvas.width * (1 - ph), canvas.height * penh);
 
-//P(E|H)*P(H)
+    //P(E|H)*P(H)
 
-ratio = ph * peh / ( (ph * peh) + ((1-ph)*penh) ) * 100; 
-document.getElementById('inside-bar').style.width = `${ratio}%`;
-document.getElementById('librarian').textContent = `${ratio.toFixed(0)}%`;
-document.getElementById('farmer').textContent = `${100-ratio.toFixed(0)}%`;
+    ratio = ph * peh / ((ph * peh) + ((1 - ph) * penh)) * 100;
+    document.getElementById('inside-bar').style.width = `${ratio}%`;
+    document.getElementById('librarian').textContent = `${ratio.toFixed(0)}%`;
+    document.getElementById('farmer').textContent = `${100 - ratio.toFixed(0)}%`;
 }
 
 
 noUiSlider.create(slider_ph, {
     start: ph,
-    connect:[true,false],
+    connect: [true, false],
     step: 0.05,
     range: {
         'min': 0,
@@ -59,18 +59,18 @@ noUiSlider.create(slider_ph, {
     },
     tooltips: true,
     format: wNumb({
-        prefix: 'P(librarian): '
+        prefix: 'P(chill): '
     }),
 });
 
 slider_ph.noUiSlider.on('update', (values) => {
     ph = values[0].split(" ")[1];
     draw();
-  });
+});
 
 noUiSlider.create(slider_peh, {
     start: peh,
-    connect:[true,false],
+    connect: [true, false],
     step: 0.1,
     range: {
         'min': 0,
@@ -80,18 +80,18 @@ noUiSlider.create(slider_peh, {
     direction: 'rtl',
     orientation: 'vertical',
     format: wNumb({
-        prefix: 'P(shy|librarian): '
+        prefix: 'P(netflix|chill): '
     }),
 });
 
 slider_peh.noUiSlider.on('update', (values) => {
     peh = values[0].split(" ")[1];
     draw();
-  });
+});
 
 noUiSlider.create(slider_penh, {
     start: penh,
-    connect:[true,false],
+    connect: [true, false],
     step: 0.1,
     range: {
         'min': 0,
@@ -101,18 +101,18 @@ noUiSlider.create(slider_penh, {
     direction: 'rtl',
     orientation: 'vertical',
     format: wNumb({
-        prefix: 'P(shy|farmer): '
+        prefix: 'P(netflix|friend): '
     }),
 });
 
 slider_penh.noUiSlider.on('update', (values) => {
     penh = values[0].split(" ")[1];
     draw();
-  });
+});
 
 for (let i = 1; i < 11; i++) {
     document.getElementById('people-grid').innerHTML += `<div class="person-row"></div>`
-    for (let j = 0; j < 20 ; j++) {
+    for (let j = 0; j < 20; j++) {
         document.getElementById('people-grid').lastElementChild.innerHTML += `
         <div class="icon-person">
         <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="male" class="svg-inline--fa fa-male fa-w-6" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 192 512"><path fill="currentColor" d="M96 0c35.346 0 64 28.654 64 64s-28.654 64-64 64-64-28.654-64-64S60.654 0 96 0m48 144h-11.36c-22.711 10.443-49.59 10.894-73.28 0H48c-26.51 0-48 21.49-48 48v136c0 13.255 10.745 24 24 24h16v136c0 13.255 10.745 24 24 24h64c13.255 0 24-10.745 24-24V352h16c13.255 0 24-10.745 24-24V192c0-26.51-21.49-48-48-48z"></path></svg>
@@ -120,7 +120,7 @@ for (let i = 1; i < 11; i++) {
         `
         count += 1;
     }
-    
+
 }
 
 draw();

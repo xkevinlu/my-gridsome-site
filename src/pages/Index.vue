0b231ed8eb88b4815133d49fcb7b71499e9bf822
xkevinlu <xkevinlu@gmail.com>
2020-07-27 16:31:14 -0700

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,6 +2,7 @@
   <Layout>
     <div>
       <h5>Featured Posts</h5>
+
       <b-card
         no-body
         class="overflow-hidden mb-4 mt-3 drop-shadow"
@@ -9,21 +10,19 @@
         :key="post.id"
         tag="article"
       >
-        <b-row no-gutters>
-          <b-col md="5">
-            <g-link class="no-link-color" :to="post.node.path">
-              <b-card-img :src="post.node.image" alt="Image" class="rounded-0"></b-card-img>
-            </g-link>
-          </b-col>
-          <b-col md="7">
-            <b-card-body>
-              <g-link class="no-link-color" :to="post.node.path">
+        <g-link class="no-link-color" :to="post.node.path">
+          <b-row no-gutters>
+            <b-col md="5">
+              <b-card-img :src="post.node.image" alt="Image" class="rounded-0 p-3"></b-card-img>
+            </b-col>
+            <b-col md="7">
+              <b-card-body>
                 <b-card-title>{{post.node.title}}</b-card-title>
-              </g-link>
-              <b-card-text>{{post.node.date}}</b-card-text>
-            </b-card-body>
-          </b-col>
-        </b-row>
+                <b-card-text>{{post.node.date}}</b-card-text>
+              </b-card-body>
+            </b-col>
+          </b-row>
+        </g-link>
       </b-card>
     </div>
   </Layout>
@@ -48,22 +47,31 @@ query blogPosts {
 
 
 <script>
-
 export default {
   metaInfo: {
     title: "Home"
-  },
+  }
 };
 </script>
 
 <style>
 .drop-shadow {
-  box-shadow:
-  0 1px 1px rgba(0, 0, 0, 0.034),
-  0 3px 2px rgba(0, 0, 0, 0.06),
-  0 5px 4px rgba(0, 0, 0, 0.056)
-;
+  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.034);
+  background: white;
+  border-radius: 5px;
+  transition: all 0.5s;
+}
 
+.drop-shadow:hover {
+  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.034), 0 3px 2px rgba(0, 0, 0, 0.06), 0 5px 4px rgba(0, 0, 0, 0.056);
+}
+
+.no-link-color {
+  color: black;
+}
+
+.no-link-color:hover {
+  text-decoration: none;
   background: white;
   border-radius: 5px;
 }

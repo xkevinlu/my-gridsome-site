23838812bb3846e386acc67aa3cd5e14be5d6449
xkevinlu <xkevinlu@gmail.com>
2020-08-28 01:05:40 -0700

    Update heatmap.js

--- static/tvshows-assets/heatmap.js
+++ static/tvshows-assets/heatmap.js
@@ -10,9 +10,9 @@ Highcharts.chart('container', {
   chart: {
     type: 'heatmap',
     marginTop: 25,
-    marginBottom: 80,
-    marginLeft:50,
-    marginRight:50,
+    marginBottom:50,
+    marginLeft:40,
+    marginRight:0,
     plotBorderWidth: 1,
     height: chartHeight,
   },
@@ -27,17 +27,13 @@ Highcharts.chart('container', {
   xAxis: {
     opposite:true,
     categories: ['S1', 'S2', 'S3', 'S4', 'S5','S6', 'S7', 'S8', 'S9', 'S10'],
-    title: {
-      enabled: false,
-    },
+    title: null,
   },
 
   yAxis: {
-    reversed:true,
+    reversed: true,
     categories: ['E1', 'E2', 'E3', 'E4', 'E5', 'E6', 'E7', 'E8', 'E9', 'E10', 'E11', 'E12', 'E13', 'E14', 'E15', 'E16', 'E17', 'E18', 'E19', 'E20', 'E21', 'E22', 'E23', 'E24', 'E25', 'E26', 'E27', 'E28', 'E29', 'E30'],
-    title: {
-      enabled: false,
-    },
+    title: null,
   },
 
   accessibility: {
@@ -92,17 +88,8 @@ Highcharts.chart('container', {
     rules: [{
       condition: {
         maxWidth: 500,
-      },
-      chartOptions: {
-        yAxis: {
-          labels: {
-            formatter: function () {
-              return this.value.charAt(0);
-            }
-          }
-        }
       }
-    }]
+    }],
   }
 
 });

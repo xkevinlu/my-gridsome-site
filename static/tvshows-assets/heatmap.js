66724fc1d74c1b87472649b9f3e984a794ec7aee
xkevinlu <xkevinlu@gmail.com>
2020-08-28 00:35:56 -0700

    Update heatmap.js

--- static/tvshows-assets/heatmap.js
+++ static/tvshows-assets/heatmap.js
@@ -27,11 +27,17 @@ Highcharts.chart('container', {
   xAxis: {
     opposite:true,
     categories: ['S1', 'S2', 'S3', 'S4', 'S5','S6', 'S7', 'S8', 'S9', 'S10'],
+    title: {
+      enabled: false,
+    },
   },
 
   yAxis: {
     reversed:true,
     categories: ['E1', 'E2', 'E3', 'E4', 'E5', 'E6', 'E7', 'E8', 'E9', 'E10', 'E11', 'E12', 'E13', 'E14', 'E15', 'E16', 'E17', 'E18', 'E19', 'E20', 'E21', 'E22', 'E23', 'E24', 'E25', 'E26', 'E27', 'E28', 'E29', 'E30'],
+    title: {
+      enabled: false,
+    },
   },
 
   accessibility: {
